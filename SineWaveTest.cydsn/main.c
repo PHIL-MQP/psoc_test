cb443c2dea4febccfd103f1c7efab9c8cdf5807f
Peter Mitrano <mitranopeter@gmail.com>
2017-10-04 19:48:08 -0400

    ultrasonic chirp with square wave works
    pew pew!

--- SineWaveTest.cydsn/main.c
+++ SineWaveTest.cydsn/main.c
@@ -3,27 +3,11 @@
 int main()
 {    
     // start components
-    WaveDAC8_1_Start();
-    // Opamp_1_Start();
-	// Opamp_1_SetPower(Opamp_1_HIGHPOWER);
-    // Opamp_2_Start();
-	// Opamp_2_SetPower(Opamp_1_HIGHPOWER);
+    Clock_1_Start();
+    Clock_2_Start();
+    PWM_1_Start();
     
-    uint8_t enabled = 0;
     for(;;)
     {
-        uint8_t rx_high = RX_CMP_IN_Read();
-        
-        if (!enabled && rx_high) {
-            enabled = 1;
-            WaveDAC8_1_Enable();
-            LED_OUT_Write(1);
-        }
-        else if (!rx_high) {
-            enabled = 0;
-            WaveDAC8_1_Stop();
-            LED_OUT_Write(0);
-        }
-        
     }
 }
