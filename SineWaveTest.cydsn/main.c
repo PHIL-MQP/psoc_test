31b49271e950e9e0d734feba469a4f7654d80ed1
Peter Mitrano <mitranopeter@gmail.com>
2017-10-03 22:26:04 -0400

    software led worked
    put in ADC but didn't test
    saving work before switching to comparator approach
    generally just trying to RF detect pulses

--- SineWaveTest.cydsn/main.c
+++ SineWaveTest.cydsn/main.c
@@ -1,12 +1,17 @@
 #include <project.h>
 
 int main()
-{
+{    
+    // start components
     WaveDAC8_1_Start();
     Opamp_1_Start();
 	Opamp_1_SetPower(Opamp_1_HIGHPOWER);
     Opamp_2_Start();
 	Opamp_2_SetPower(Opamp_1_HIGHPOWER);
+    ADC_DelSig_1_Start();  
+    ADC_DelSig_1_StartConvert();
     
-    for(;;);
+    for(;;)
+    {
+    }
 }
