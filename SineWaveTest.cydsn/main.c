32096bb1e442a9a6cc83334f0e7d5ed926164f0a
Peter Mitrano <mitranopeter@gmail.com>
2017-10-03 22:52:05 -0400

    still seeing lots of noise (looks like it's floating)
    the Rx is clearly high when it's high though.

--- SineWaveTest.cydsn/main.c
+++ SineWaveTest.cydsn/main.c
@@ -4,14 +4,25 @@ int main()
 {    
     // start components
     WaveDAC8_1_Start();
-    Opamp_1_Start();
-	Opamp_1_SetPower(Opamp_1_HIGHPOWER);
-    Opamp_2_Start();
-	Opamp_2_SetPower(Opamp_1_HIGHPOWER);
-    ADC_DelSig_1_Start();  
-    ADC_DelSig_1_StartConvert();
+    // Opamp_1_Start();
+	// Opamp_1_SetPower(Opamp_1_HIGHPOWER);
+    // Opamp_2_Start();
+	// Opamp_2_SetPower(Opamp_1_HIGHPOWER);
     
+    uint8_t enabled = 0;
     for(;;)
     {
+        uint8_t rx_high = RX_CMP_IN_Read();
+        if (!enabled && rx_high) {
+            enabled = 1;
+            WaveDAC8_1_Enable();
+            LED_OUT_Write(1);
+        }
+        else {
+            enabled = 0;
+            WaveDAC8_1_Stop();
+            LED_OUT_Write(0);
+        }
+        
     }
 }
